b2f7688e8be4222a2fa09e1d5ac635463537f359
Kilahcim <krzysztof.michalik01@gmail.com>
2018-02-16 11:42:50 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/breakpoints.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="  crossorigin="anonymous"></script>
+    <script src="bootstrap-3.3.7-dist/js/bootstrap.js"></script>
+    <title>Bootstrap details training</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <p>MENU</p>
+
+          </button>
+          <a class="navbar-brand" href="#">
+            <img src="images/logo.png" alt="Logo">
+            <p>Capture</p>
+          </a>
+
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li><a href="#">About</a></li>
+            <li><a href="#">Features</a></li>
+            <li><a href="#">Testimonial</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li>
+              <div class="social">
+                <a href="#" class="fa fa-facebook"></a>
+              </div>
+            </li>
+            <li>
+              <div class="social">
+                <a href="#" class="fa fa-twitter"></a>
+              </div>
+            </li>
+            <li>
+              <div class="social">
+                <a href="#" class="fa fa-google"></a>
+              </div>
+            </li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+    <section class="background">
+      <div class="container-fluid">
+        <div class="row">
+          <img src="images/background.jpg" class="responsive-img"alt="backgrond">
+        </div>
+      </div>
+    </section>
+
+  </body>
+</html>
