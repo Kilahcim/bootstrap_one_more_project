61b48416e36ec6c9858fa4d2a6f154b514333c7f
Kilahcim <krzysztof.michalik01@gmail.com>
2018-02-21 13:07:07 +0100

    final commit

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/breakpoints.css">
@@ -14,75 +14,70 @@
     <title>Bootstrap details training</title>
   </head>
   <body>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="img-background">
-          <nav class="navbar navbar-default">
-            <div class="container">
-              <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                  <span class="sr-only">Toggle navigation</span>
-                  <p>MENU</p>
+    <div class="img-background">
+      <nav class="navbar navbar-default">
+        <div class="container navbarposition">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <p>MENU</p>
 
-                </button>
-                <a class="navbar-brand" href="#">
-                  <img src="images/logo.png" alt="Logo">
-                  <p>Capture</p>
-                </a>
+            </button>
+            <a class="navbar-brand" href="#">
+              <img src="images/logo.png" alt="Logo">
+              <p>Capture</p>
+            </a>
 
-              </div>
-              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                  <li><a href="#">About</a></li>
-                  <li><a href="#">Features</a></li>
-                  <li><a href="#">Testimonial</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                  <li>
-                    <div class="social">
-                      <i class="fab fa-facebook-f"></i>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="social">
-                      <i class="fab fa-twitter"></i>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="social">
-                      <i class="fab fa-google"></i>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-          <section class="header">
-            <div class="container">
-              <div class="row">
-                <div class="col-xs-12 col-md-6">
-                  <h1>TAKE PHOTO LIKE A PRO.</h1>
-                  <p>whit only your iphone and capture app you can take stunning image like a proffesional photographer</p>
+          </div>
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+              <li><a href="#">About</a></li>
+              <li><a href="#">Features</a></li>
+              <li><a href="#">Testimonial</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li>
+                <div class="social">
+                  <i class="fab fa-facebook-f"></i>
                 </div>
-              </div>
-                <div class="col-xs-12 col-md-3">
-                  <div class="border">
-                    <i class="fab fa-apple" style="font-size: 24px"></i>
-                    <span>IPHONE APP</span>
-                  </div>
+              </li>
+              <li>
+                <div class="social">
+                  <i class="fab fa-twitter"></i>
                 </div>
-                <div class="col-xs-12 col-md-3">
-                  <div class="border">
-                    <i class="fab fa-android" style="font-size: 24px"></i>
-                    <span>ANDROID APP</span>
-                  </div>
+              </li>
+              <li>
+                <div class="social">
+                  <i class="fab fa-google"></i>
                 </div>
-              </div>
-            </div>
-          </section>
+              </li>
+            </ul>
+          </div>
         </div>
+      </nav>
+      <section class="header">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12 col-md-6">
+              <h1>TAKE PHOTO LIKE A PRO.</h1>
+              <p>whit only your iphone and capture app you can take stunning image like a proffesional photographer</p>
+            </div>
+          </div>
+            <div class="col-xs-12 col-md-3">
+              <div class="border">
+                <i class="fab fa-apple" style="font-size: 24px"></i>
+                <span>IPHONE APP</span>
+              </div>
+            </div>
+            <div class="col-xs-12 col-md-3">
+              <div class="border">
+                <i class="fab fa-android" style="font-size: 24px"></i>
+                <span>ANDROID APP</span>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
-    </div>
 
     <section class="partners">
       <div class="container">
@@ -105,14 +100,14 @@
     <section class="description gray">
       <div class="container">
         <div class="row">
-          <div class="col-xs-12 col-md-6">
+          <div class="col-xs-12 col-md-6 text-center">
             <img  class="responsive-image" src="images/iphone.png" alt="iphone">
           </div>
           <div class="col-xs-12 col-md-6 visibilite">
             <h2 class="text-center">CAPTURE UNFORGETTABLE MOMENTS.</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p>
-            <ul>
+            <ul class="position-center">
               <li><div class="eye"><i class="fas fa-eye"></i></div>Eusimod ilgula ipsum vulputate tellus.</li>
               <li><div class="eye"><i class="fas fa-eye"></i></div>Eusimod ilgula ipsum vulputate tellus.</li>
               <li><div class="eye"><i class="fas fa-eye"></i></div>Eusimod ilgula ipsum vulputate tellus.</li>
@@ -121,7 +116,7 @@
             </ul>
             <div class="border text-center">
               <i class="fa fa-play"></i>
-              <span>ABOUT VIDEO</span>
+              <span class="last">ABOUT VIDEO</span>
             </div>
             <p>get the app</p>
           </div>
@@ -131,47 +126,59 @@
     <section class="green fourth">
       <div class="container">
         <div class="row">
-          <div class="col-xs-12 col-md-4 text-center">
-            <div class="icon">
-              <i class="fa fa-users"></i>
+          <div class="box">
+            <div class="col-xs-12 col-md-4 text-center">
+              <div class="icon first padding">
+                <i class="fa fa-users"></i>
+              </div>
+              <h3>MALESUADA FAMES TURPIS</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dol</p>
             </div>
-            <h3>MALESUADA FAMES TURPIS</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dol</p>
           </div>
-          <div class="col-xs-12 col-md-4 text-center">
-            <div class="icon">
-              <i class="fa fa-users"></i>
+          <div class="box">
+            <div class="col-xs-12 col-md-4 text-center">
+              <div class="icon padding">
+                <i class="fa fa-users"></i>
+              </div>
+              <h3>MALESUADA FAMES TURPIS</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dol</p>
             </div>
-            <h3>MALESUADA FAMES TURPIS</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dol</p>
           </div>
-          <div class="col-xs-12 col-md-4 text-center">
-            <div class="icon">
-              <i class="fa fa-users"></i>
+          <div class="box">
+            <div class="col-xs-12 col-md-4 text-center">
+              <div class="icon padding">
+                <i class="fa fa-users"></i>
+              </div>
+              <h3>MALESUADA FAMES TURPIS</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dol</p>
             </div>
-            <h3>MALESUADA FAMES TURPIS</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dol</p>
           </div>
-          <div class="col-xs-12 col-md-4 text-center">
-            <div class="icon">
-              <i class="fa fa-users"></i>
+          <div class="box">
+            <div class="col-xs-12 col-md-4 text-center">
+              <div class="icon">
+                <i class="fa fa-users"></i>
+              </div>
+              <h3>MALESUADA FAMES TURPIS</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dol</p>
             </div>
-            <h3>MALESUADA FAMES TURPIS</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dol</p>
           </div>
-          <div class="col-xs-12 col-md-4 text-center">
-            <div class="icon">
-              <i class="fa fa-users"></i>
+          <div class="box">
+            <div class="col-xs-12 col-md-4 text-center">
+              <div class="icon">
+                <i class="fa fa-users"></i>
+              </div>
+              <h3>MALESUADA FAMES TURPIS</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dol</p>
             </div>
-            <h3>MALESUADA FAMES TURPIS</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dol</p>
           </div>
-          <div class="col-xs-12 col-md-4 text-center">
-            <div class="icon">
-              <i class="fa fa-users"></i>
+          <div class="box">
+            <div class="col-xs-12 col-md-4 text-center">
+              <div class="icon">
+                <i class="fa fa-users"></i>
+              </div>
+              <h3>MALESUADA FAMES TURPIS</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dol</p>
             </div>
-            <h3>MALESUADA FAMES TURPIS</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dol</p>
           </div>
         </div>
       </div>
@@ -183,7 +190,6 @@
             <div id="myCarousel" class="carousel slide" data-ride="carousel">
               <div class="carousel-inner" role="listbox">
                 <div class="item active">
-                  <img class="first-slide" src="images/sliderbackground.jpg" alt="First slide">
                   <div class="container">
                     <div class="carousel-caption">
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -193,7 +199,6 @@
                   </div>
                 </div>
                 <div class="item">
-                  <img class="first-slide" src="images/sliderbackground.jpg" alt="First slide">
                   <div class="container">
                     <div class="carousel-caption">
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
