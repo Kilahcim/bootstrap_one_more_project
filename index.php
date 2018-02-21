9dba09733cda5e73156b2c71a5aaa6f833c2b92c
Kilahcim <krzysztof.michalik01@gmail.com>
2018-02-21 07:58:33 +0100

    ready project

--- index.php
+++ index.php
@@ -10,58 +10,248 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="  crossorigin="anonymous"></script>
     <script src="bootstrap-3.3.7-dist/js/bootstrap.js"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
     <title>Bootstrap details training</title>
   </head>
   <body>
-    <nav class="navbar navbar-default">
-      <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            <p>MENU</p>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="img-background">
+          <nav class="navbar navbar-default">
+            <div class="container">
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                  <span class="sr-only">Toggle navigation</span>
+                  <p>MENU</p>
 
-          </button>
-          <a class="navbar-brand" href="#">
-            <img src="images/logo.png" alt="Logo">
-            <p>Capture</p>
-          </a>
+                </button>
+                <a class="navbar-brand" href="#">
+                  <img src="images/logo.png" alt="Logo">
+                  <p>Capture</p>
+                </a>
 
+              </div>
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                  <li><a href="#">About</a></li>
+                  <li><a href="#">Features</a></li>
+                  <li><a href="#">Testimonial</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                  <li>
+                    <div class="social">
+                      <i class="fab fa-facebook-f"></i>
+                    </div>
+                  </li>
+                  <li>
+                    <div class="social">
+                      <i class="fab fa-twitter"></i>
+                    </div>
+                  </li>
+                  <li>
+                    <div class="social">
+                      <i class="fab fa-google"></i>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+          <section class="header">
+            <div class="container">
+              <div class="row">
+                <div class="col-xs-12 col-md-6">
+                  <h1>TAKE PHOTO LIKE A PRO.</h1>
+                  <p>whit only your iphone and capture app you can take stunning image like a proffesional photographer</p>
+                </div>
+              </div>
+                <div class="col-xs-12 col-md-3">
+                  <div class="border">
+                    <i class="fab fa-apple" style="font-size: 24px"></i>
+                    <span>IPHONE APP</span>
+                  </div>
+                </div>
+                <div class="col-xs-12 col-md-3">
+                  <div class="border">
+                    <i class="fab fa-android" style="font-size: 24px"></i>
+                    <span>ANDROID APP</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
         </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav">
-            <li><a href="#">About</a></li>
-            <li><a href="#">Features</a></li>
-            <li><a href="#">Testimonial</a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li>
-              <div class="social">
-                <a href="#" class="fa fa-facebook"></a>
-              </div>
-            </li>
-            <li>
-              <div class="social">
-                <a href="#" class="fa fa-twitter"></a>
-              </div>
-            </li>
-            <li>
-              <div class="social">
-                <a href="#" class="fa fa-google"></a>
-              </div>
-            </li>
-          </ul>
-        </div><!-- /.navbar-collapse -->
-      </div><!-- /.container-fluid -->
-    </nav>
-    <section class="background">
-      <div class="container-fluid">
+      </div>
+    </div>
+
+    <section class="partners">
+      <div class="container">
         <div class="row">
-          <img src="images/background.jpg" class="responsive-img"alt="backgrond">
+          <div class="col-xs-6 text-center col-md-3">
+            <img src="images/google.png" alt="partners-logo">
+          </div>
+          <div class="col-xs-6 text-center col-md-3">
+            <img src="images/facebook.png" alt="partners-logo">
+          </div>
+          <div class="col-xs-6 text-center col-md-3">
+            <img src="images/yahho.png" alt="partners-logo">
+          </div>
+          <div class="col-xs-6 text-center col-md-3">
+            <img src="images/paypal-logo.png" alt="partners-logo">
+          </div>
         </div>
       </div>
     </section>
-
+    <section class="description gray">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-md-6">
+            <img  class="responsive-image" src="images/iphone.png" alt="iphone">
+          </div>
+          <div class="col-xs-12 col-md-6 visibilite">
+            <h2 class="text-center">CAPTURE UNFORGETTABLE MOMENTS.</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p>
+            <ul>
+              <li><div class="eye"><i class="fas fa-eye"></i></div>Eusimod ilgula ipsum vulputate tellus.</li>
+              <li><div class="eye"><i class="fas fa-eye"></i></div>Eusimod ilgula ipsum vulputate tellus.</li>
+              <li><div class="eye"><i class="fas fa-eye"></i></div>Eusimod ilgula ipsum vulputate tellus.</li>
+              <li><div class="eye"><i class="fas fa-eye"></i></div>Eusimod ilgula ipsum vulputate tellus.</li>
+              <li><div class="eye"><i class="fas fa-eye"></i></div>Eusimod ilgula ipsum vulputate tellus.</li>
+            </ul>
+            <div class="border text-center">
+              <i class="fa fa-play"></i>
+              <span>ABOUT VIDEO</span>
+            </div>
+            <p>get the app</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="green fourth">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-md-4 text-center">
+            <div class="icon">
+              <i class="fa fa-users"></i>
+            </div>
+            <h3>MALESUADA FAMES TURPIS</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dol</p>
+          </div>
+          <div class="col-xs-12 col-md-4 text-center">
+            <div class="icon">
+              <i class="fa fa-users"></i>
+            </div>
+            <h3>MALESUADA FAMES TURPIS</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dol</p>
+          </div>
+          <div class="col-xs-12 col-md-4 text-center">
+            <div class="icon">
+              <i class="fa fa-users"></i>
+            </div>
+            <h3>MALESUADA FAMES TURPIS</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dol</p>
+          </div>
+          <div class="col-xs-12 col-md-4 text-center">
+            <div class="icon">
+              <i class="fa fa-users"></i>
+            </div>
+            <h3>MALESUADA FAMES TURPIS</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dol</p>
+          </div>
+          <div class="col-xs-12 col-md-4 text-center">
+            <div class="icon">
+              <i class="fa fa-users"></i>
+            </div>
+            <h3>MALESUADA FAMES TURPIS</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dol</p>
+          </div>
+          <div class="col-xs-12 col-md-4 text-center">
+            <div class="icon">
+              <i class="fa fa-users"></i>
+            </div>
+            <h3>MALESUADA FAMES TURPIS</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dol</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="gray fifth">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+              <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                  <img class="first-slide" src="images/sliderbackground.jpg" alt="First slide">
+                  <div class="container">
+                    <div class="carousel-caption">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                      <h4>ALEX FREDY</h4>
+                      <p>Android Developer</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="item">
+                  <img class="first-slide" src="images/sliderbackground.jpg" alt="First slide">
+                  <div class="container">
+                    <div class="carousel-caption">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                      <h4>ALEX FREDY</h4>
+                      <p>Android Developer</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true">
+                </span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true">
+                </span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="green sixth">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <p>Join Our Liste and stay updated!</p>
+          </div>
+          <form class="register text-center" action="index.html" method="post">
+            <input type="text" name="email" value="" placeholder="Enter your email">
+            <input type="text" name="username" value="" placeholder="Enter your name">
+            <input type="number" name="phone" value="" placeholder="Enter your phone number">
+            <div class="send">
+              <i class="far fa-envelope"></i>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+    <footer class="gray seventh">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 text-center">
+            <div class="oslogos">
+              <ul>
+                <li><i class="fab fa-android"></i></li>
+                <li><i class="fab fa-apple"></i></li>
+                <li><i class="fab fa-windows"></i></li>
+              </ul>
+            </div>
+            <div class="col-xs-12">
+              <p>&#9400; 2015 <b>Pixelhing.com</b>. All Rights Reserved</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   </body>
 </html>
